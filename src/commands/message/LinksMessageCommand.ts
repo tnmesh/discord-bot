d2f1ceb9dc38f184f012c84514a1c22c196e7305
nortoh <c@hrtn.dev>
2026-01-13 22:08:48 -0600

    feat: add \!links to show all available links

---
+++ src/commands/message/LinksMessageCommand.ts
@@ -0,0 +1,43 @@
+import { EmbedBuilder, Guild, Message, TextChannel } from "discord.js";
+import CommandMessage from "./CommandMessage";
+import meshDB from "MeshDB";
+import { Pagination } from "pagination.djs";
+
+export default class LinksMessageCommand extends CommandMessage {
+
+    /** {@inheritdoc} */
+    constructor() {
+        super('links');
+    }
+
+
+    /** {@inheritdoc} */
+    public async handle(guild: Guild | null, commandArgs: string[], message: Message): Promise<void> {
+        if (guild === null) {
+            return;
+        }
+
+        const links = await meshDB.client.links.findMany({
+            distinct: ['type'],
+            select: {
+                type: true,
+            }
+        });
+
+        const fields = [];
+        links.forEach((link) => {
+            fields.push({
+                name: `!${link.type}`,
+                value: '',
+            })
+        })
+
+        // let channel: TextChannel = <TextChannel>message.channel;
+
+        const pagination = new Pagination(message);
+        pagination.setFields(fields);
+        pagination.setTitle('Available Links')
+        pagination.paginateFields();
+        pagination.render();
+    }
+}
